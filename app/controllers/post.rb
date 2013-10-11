a3d3e5143ebac86d6a091911f41a9c60fa00f28d
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-10 21:43:55 -0500

    added body in post and cleaned up new_post, posts, and index

--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -8,3 +8,18 @@ get '/posts/:id' do
   @post = Post.find(params[:id])
   erb :post
 end
+
+
+get '/newpost' do
+
+  erb :new_post
+end
+
+
+
+post '/newpost' do
+  @new_post = Post.create(params[:listing])
+  @new_tag = Tag.create(title: params[:tag])
+  @new_post.tags << @new_tag
+  redirect to '/'
+end
